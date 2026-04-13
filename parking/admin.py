62284c1b9076c70f5ec0e83e70ceaaa0901be4a6
gantapriya55 <105838846+gantapriya55@users.noreply.github.com>
2026-04-12 22:48:17 -0400

    Delete parking directory

--- parking/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-admin.site.register(ParkingLot)
-admin.site.register(Floor)
-admin.site.register(ParkingSlot)
-admin.site.register(Vehicle)
-admin.site.register(PricingRule)
-admin.site.register(ParkingTicket)
